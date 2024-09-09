57bc6d9d2290377c98ac7c19c3065f8b5425429e
BIJIT DEB <bijitdeb0703@gmail.com>
2024-09-10 00:38:19 +0600

    done

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
-let h = document.querySelector(".h1")
 
-console.log(h);
+let icon = document.querySelector(".i1")
+let icon2 = document.querySelector(".i2")
+
+let icon3 = document.querySelector(".i3")
+let icon4 = document.querySelector(".i4")
+
+let icon5 = document.querySelector(".i5")
+let icon6 = document.querySelector(".i6")
+
+icon.addEventListener("click", function(){
+    icon.style.display = "none"
+    icon2.style.display = "block"
+})
+icon3.addEventListener("click", function(){
+    icon3.style.display = "none"
+    icon4.style.display = "block"
+})
+icon5.addEventListener("click", function(){
+    icon5.style.display = "none"
+    icon6.style.display = "block"
+})
